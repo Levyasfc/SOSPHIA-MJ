be3bbf3fbc2154a3205a534064d026dab9a3e12a
Santiago Lozada <148565532+Levyasfc@users.noreply.github.com>
2025-11-19 04:36:03 +0000

    CAMBIOS

--- app/main.py
+++ app/main.py
@@ -11,6 +11,10 @@ app = FastAPI(title="Servicio MODULOJurídico SOSPHIA", version="3.0")
 
 #Base.metadata.create_all(bind=engine)
 
+@app.get("/")
+def home():
+    return {"message": "OK"}
+
 @app.get("/", response_class=HTMLResponse)
 def home():
     return """
